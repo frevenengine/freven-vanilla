e1ecb2196d324933fe899ea975a8ded2f7e5e620
ogyrec <ogyrec.off@gmail.com>
2026-05-15 00:01:13 +0200

    Use automatic debug block materials (#24)

--- crates/freven_vanilla_essentials/src/blocks.rs
+++ crates/freven_vanilla_essentials/src/blocks.rs
@@ -1,4 +1,4 @@
-use freven_block_sdk_types::{BlockDescriptor, RenderLayer};
+use freven_block_sdk_types::BlockDescriptor;
 
 pub(crate) const STONE_KEY: &str = "freven.vanilla:stone";
 pub(crate) const DIRT_KEY: &str = "freven.vanilla:dirt";
@@ -6,15 +6,15 @@ pub(crate) const GRASS_KEY: &str = "freven.vanilla:grass";
 
 #[inline]
 pub(crate) fn stone_def() -> BlockDescriptor {
-    BlockDescriptor::new(true, true, RenderLayer::Opaque, 0x8080_80FF, 1)
+    BlockDescriptor::solid_colored_cube(0x8080_80FF)
 }
 
 #[inline]
 pub(crate) fn dirt_def() -> BlockDescriptor {
-    BlockDescriptor::new(true, true, RenderLayer::Opaque, 0x6B4F_2AFF, 2)
+    BlockDescriptor::solid_colored_cube(0x6B4F_2AFF)
 }
 
 #[inline]
 pub(crate) fn grass_def() -> BlockDescriptor {
-    BlockDescriptor::new(true, true, RenderLayer::Opaque, 0x3FA3_4DFF, 3)
+    BlockDescriptor::solid_colored_cube(0x3FA3_4DFF)
 }
